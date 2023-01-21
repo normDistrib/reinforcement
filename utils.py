720670253b0d96c1baeddb26961e789c689d228b
User Name <alordofmountain@gmail.com>
2023-01-22 00:01:42 +0100

    Added % optimal actions plot

--- utils.py
+++ utils.py
@@ -13,36 +13,40 @@ def greedy(n,ts,tasks,e=0.01):
         - e: probability of choosing randomly between actions:
 
     Returns:
-        Rewards averaged between total tasks.
+        Rewards averaged between total tasks, % Optimal Solutions.
 
     """
 
     total_rewards = np.zeros(ts)
+    optimal = np.zeros(ts)
 
     for _ in range(tasks):
         vals = np.random.randn(n) # real values
+        opt = np.argmax(vals)
         N = np.ones(n) # number of time selected an action
         estimates = np.zeros(n) # estimations of values for each action
-        rewards = [] # rewards obtained at each step
 
         pos = np.random.randint(0,n)
+        if pos == opt:
+            optimal[0]+=1
+
         R = vals[pos] + np.random.randn(1)[0] # R_k = q(A) + random_noice
         estimates[pos] = estimates[pos]+1/N[pos]*(R-estimates[pos])
-        rewards.append(R)
+        total_rewards[0]+=R
         N[pos] += 1
 
-        for _ in range(1,ts):
+        for t in range(1,ts):
             pos = np.random.choice([np.random.randint(0,n),np.argmax(estimates)],
                                    size = 1,
                                    p = [e,1-e])[0]
             N[pos] += 1
             R = vals[pos] + np.random.randn(1)[0]
+            if pos == opt:
+                optimal[t]+=1
             estimates[pos] = estimates[pos]+1/N[pos]*(R-estimates[pos])
-            rewards.append(R)
+            total_rewards[t]+=R
 
-        total_rewards += np.array(rewards)
-
-    return total_rewards/tasks
+    return total_rewards/tasks, optimal/tasks
 
 def UCB(n,ts,tasks,c):
     """
@@ -60,40 +64,54 @@ def UCB(n,ts,tasks,c):
         - c: hyper-parameter to control uncertainty weight.
 
     Returns:
-        Rewards averaged between total tasks.
+        Rewards averaged between total tasks, % Optimal Solutions.
 
     """
 
     total_rewards = np.zeros(ts)
+    optimal = np.zeros(ts)
 
     for _ in range(tasks):
         vals = np.random.randn(n) # real values
+        opt = np.argmax(vals)
         N = np.ones(n) # number of time selected an action
         estimates = np.zeros(n) # estimations of values for each action
-        rewards = [] # rewards obtained at each step
 
         pos = np.random.randint(0,n)
         R = vals[pos] + np.random.randn(1)[0] # R_k = q(A) + random_noice
+        if pos == opt:
+            optimal[0]+=1
         estimates[pos] = estimates[pos]+1/N[pos]*(R-estimates[pos])
-        rewards.append(R)
+        total_rewards[0]+=R
         N[pos] += 1
 
         for t in range(1,ts):
             pos = np.argmax(estimates + c*np.sqrt(np.log(t)/N))
             N[pos] += 1
             R = vals[pos] + np.random.randn(1)[0]
+            if pos == opt:
+                optimal[t]+=1
             estimates[pos] = estimates[pos]+1/N[pos]*(R-estimates[pos])
-            rewards.append(R)
+            total_rewards[t]+=R
 
-        total_rewards += np.array(rewards)
-
-    return total_rewards/tasks
+    return total_rewards/tasks, optimal/tasks
 
 
 if __name__ == '__main__':
-    #plt.plot(greedy(10,1000,500,0)) # greedy method
-    #plt.plot(greedy(10,1000,500,e=0.01))
-    plt.plot(greedy(10,1000,500,e=0.1),label='e-greedy. e = 0.1')
-    plt.plot(UCB(10,1000,500,c=2),label='UCB. c = 2')
+
+    fig, axs = plt.subplots(2,1)
+    greedy_avg, greedy_optimal = greedy(10,1000,500,e=0)
+    e_greedy_avg, e_greedy_optimal = greedy(10,1000,500,e=0.1)
+    ucb_avg, ucb_optimal = UCB(10,1000,500,c=2)
+    axs[0].plot(greedy_avg,label='greedy. e = 0',linewidth=1) # greedy method
+    axs[0].plot(e_greedy_avg,label='e-greedy. e = 0.1',linewidth=1) # e-greedy method
+    axs[0].plot(ucb_avg,label='UCB. c = 2',linewidth=1) # UCB method
+
+    axs[1].plot(greedy_optimal,label='greedy. e = 0',linewidth=1) # greedy method
+    axs[1].plot(e_greedy_optimal,label='e-greedy. e = 0.1',linewidth=1) # e-greedy method
+    axs[1].plot(ucb_optimal,label='UCB. c = 2',linewidth=1) # UCB method
+
+    axs[1].set_ylim([0,1])
+
     plt.legend()
     plt.show()
