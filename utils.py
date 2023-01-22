8f9efd8c9075d161ea17d89cae02de5a8af71ddc
User Name <alordofmountain@gmail.com>
2023-01-22 16:44:17 +0100

    Gradient Bandit

--- utils.py
+++ utils.py
@@ -96,9 +96,90 @@ def UCB(n,ts,tasks,c):
 
     return total_rewards/tasks, optimal/tasks
 
+def gradient(n,ts,tasks,alpha=0.1,base=True):
+    """
+    Gradient Bandit.
+
+    P(choosing a) = preference(a)/sum(preference(b)) for all b!=a
+    Preferences are updated using stochastic aproximation of gradient ascent:
+        H_t+1(a) = H_t(a) + alpha*(R_t-Baseline)*(1-P(choosing a))
+        H_t+1(b) = H_t(b) - alpha*(R_t-Baseline)*(P(choosing b))
+
+    Params:
+        - n: number of actions
+        - ts: number of timesteps.
+        - tasks: number of n-Armed Bandit tasks generated.
+        - alpha: hyper-parameter controlling the step size.
+        - base: If true, using average reward as baseline. If false,
+                baseline = 0.
+
+    Returns:
+        % Optimal Solutions.
+
+    """
+
+    def _get_pi(pos):
+        return np.exp(preferences[pos])/np.exp(preferences).sum()
+
+    optimal = np.zeros(ts)
+
+    for _ in range(tasks):
+        vals = np.random.normal(loc=4,size=n) # real values
+        opt = np.argmax(vals)
+        N = 1 # number of time selected an action
+        baseline = 0 # baselines (always zero or average of rewards)
+        preferences = np.zeros(n)
+        probs = np.exp(preferences)
+        probs /= np.sum(probs)
+
+        pos = np.random.choice(n, size = 1, p = probs)[0]
+        R = vals[pos] + np.random.randn(1)[0] # R_k = q(A) + random_noice
+        diff = R - baseline
+        if pos == opt:
+            optimal[0]+=1
+        preferences[pos] += alpha*diff*(1-probs[pos])
+        for i in range(n):
+            if i != pos:
+                preferences[i] -= alpha*diff*probs[i]
+        if base:
+            baseline += diff/N
+        N += 1
+
+        for t in range(1,ts):
+
+            # update probabilities
+            probs = np.exp(preferences)
+            probs /= np.sum(probs)
+
+            # choose value using new distribution
+            pos = np.random.choice(n, size = 1, p = probs)[0]
+
+            # reward
+            R = vals[pos] + np.random.randn(1)[0]
+
+            diff = R - baseline
+
+            if pos == opt:
+                optimal[t]+=1
+
+            # update preferences
+            preferences[pos] += alpha*diff*(1-probs[pos])
+            for i in range(n):
+                if i != pos:
+                    preferences[i] -= alpha*diff*probs[i]
+
+            # update baseline
+            if base:
+                baseline += (R-baseline)/N
+            N += 1
+
+    return optimal/tasks
 
 if __name__ == '__main__':
 
+    """
+
+    # Greedy method vs e-Greedy method vs Upper Confidence Bound method
     fig, axs = plt.subplots(2,1)
     greedy_avg, greedy_optimal = greedy(10,1000,500,e=0)
     e_greedy_avg, e_greedy_optimal = greedy(10,1000,500,e=0.1)
@@ -114,4 +195,11 @@ if __name__ == '__main__':
     axs[1].set_ylim([0,1])
 
     plt.legend()
+    plt.show()
+    """
+
+    # Gradient Bandit
+    plt.plot(gradient(10,1000,500,alpha=0.1,base=True))
+    plt.plot(gradient(10,1000,500,alpha=0.1,base=False))
+    plt.ylim([0,1])
     plt.show()
